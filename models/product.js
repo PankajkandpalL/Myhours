a9b1d999287dcb337a30887661521747e5b94f94
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-28 04:21:32 +0530

    created category, products and offers model

---
+++ models/product.js
@@ -0,0 +1,35 @@
+import { Schema, model, models } from "mongoose";
+
+const ProductSchema = new Schema({
+	createdAt: { type: Date, default: Date.now() },
+	productName: String,
+	category: { type: Schema.Types.ObjectId, ref: "category" },
+	oldPrice: Number,
+	newPrice: Number,
+	hasColor: Boolean,
+	colors: [String],
+	imagesByColor: [
+		{
+			color: String,
+			images: [String],
+		},
+	],
+	poster: String,
+	description: String,
+	ingredients: String,
+	howToUse: String,
+	commonlyAskedQuestions: [
+		{
+			Q: String,
+			A: String,
+		},
+	],
+	reviews: [String],
+	availableOffers: [{ message: String, discount: String }],
+	isTrending: Boolean,
+	sellCount: Number,
+});
+
+const ProductModel = models.product || model("product", ProductSchema);
+
+export default ProductModel;
