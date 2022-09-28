78ebfd94a756cbc0137d2cd761ec2a7bd57de13b
cs2812 <cs786786786@gmail.com>
2022-09-28 14:41:25 +0530

    add cart

---
+++ pages/cart.js
@@ -0,0 +1,23 @@
+import { Box } from "@mui/material"
+
+const cart = () => {
+  return (
+    <Box backgroundColor="#f2f2f2" padding="2rem" >
+    <Box backgroundColor="white" borderRadius={"10px"} padding={"1rem"} display={"flex"}>
+    {/* Box 1----> */}
+    <Box>
+        <Box>
+
+        </Box>
+
+    </Box>
+
+    {/* Box 2----> */}
+    <Box></Box>
+
+    </Box>
+    </Box>
+  )
+}
+
+export default cart
