5a2cd6cbfbd1b94cb21a952e629f4afb8a888fe4
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-30 04:05:44 +0530

    api for getting product by category is ready

---
+++ pages/api/product/byCategory.js
@@ -0,0 +1,31 @@
+import dbConnect from "../../../lib/mongodbConnect";
+import CategoryModel from "../../../models/category";
+import ProductModel from "../../../models/product";
+
+const handler = async (req, res) => {
+	if (req.method !== "GET") {
+		return res.status(403).send({
+			message: `${req.method} is not supported on this route`,
+		});
+	}
+	let { category } = req.body;
+	if (!category) {
+		return res.status(404).send({
+			message: "Category is missing in body",
+		});
+	}
+	try {
+		await dbConnect();
+		let categoryData = await CategoryModel.findOne({
+			category: category.toLowerCase(),
+		});
+		let productsData = await ProductModel.find({ category: categoryData._id });
+		res.send(productsData);
+	} catch (err) {
+		res.send({
+			message: err,
+		});
+	}
+};
+
+export default handler;
