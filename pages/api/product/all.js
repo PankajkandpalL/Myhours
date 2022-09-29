318cdb811a0463789a100caf34c18c189fc4f9e8
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-30 04:04:51 +0530

    added handler for requests other than get

--- pages/api/product/all.js
+++ pages/api/product/all.js
@@ -2,6 +2,11 @@ import dbConnect from "../../../lib/mongodbConnect";
 import ProductModel from "../../../models/product";
 
 const handler = async (req, res) => {
+	if (req.method !== "GET") {
+		return res.status(403).send({
+			message: `${req.method} is not supported on this route`,
+		});
+	}
 	try {
 		await dbConnect();
 		let products = await ProductModel.find();
