f3d3affade65e84c9b708eae27d6ad90d3461ab7
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-29 22:34:21 +0530

    created dynamic routes

---
+++ pages/collections/[collection].jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const CollectionPage = () => {
+	return (
+		<Box>
+			<Typography>CollectionPage</Typography>
+		</Box>
+	);
+};
+
+export default CollectionPage;
