a6215d55cf16a6b3d146f0fa416415934029d10b
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-30 05:03:57 +0530

    connected product page with getProductByID API

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,12 +1,32 @@
 import { Box, Typography } from "@mui/material";
+import axios from "axios";
 import React from "react";
+import config from "../../config.json";
 
-const ProductPage = () => {
+let { BASE_API_URL } = config;
+
+const ProductPage = ({ product }) => {
 	return (
 		<Box>
-			<Typography>ProductPage</Typography>
+			<Typography>ProductPage {product.productName}</Typography>
 		</Box>
 	);
 };
 
 export default ProductPage;
+
+export const getServerSideProps = async (context) => {
+	const { id } = context.params;
+	let apiUrl = `${BASE_API_URL}/product/byID`;
+	let product = await axios.get(apiUrl, {
+		params: {
+			_id: id,
+		},
+	});
+	product = product.data;
+	return {
+		props: {
+			product,
+		},
+	};
+};
