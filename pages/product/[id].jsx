24b09dc8947bb0ab17ed996e2a2fc4f66b072bb6
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-30 00:01:31 +0530

    created components for dynamic routes

---
+++ pages/product/[id].jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const ProductPage = () => {
+	return (
+		<Box>
+			<Typography>ProductPage</Typography>
+		</Box>
+	);
+};
+
+export default ProductPage;
