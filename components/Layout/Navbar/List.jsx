473378ff3a41f2918956aed94dacfaf7673d7fd6
deepumandal <nomercyhunters@gmail.com>
2022-10-01 01:21:59 +0530

    data creattion

--- components/Layout/Navbar/List.jsx
+++ components/Layout/Navbar/List.jsx
@@ -1,26 +1,34 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-
+import { Box } from "@mui/material";
+import React from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import DiscountOutlinedIcon from "@mui/icons-material/DiscountOutlined";
+import Link from "next/link";
 
 const List = () => {
   return (
-    <Box 
-    sx={{
-      display :"flex",
-      justifyContent : "space-between",
-      minWidth : "90px",
-      alignItems : "center"
-    }}
-    border={"1px solid red"}>
-{/* <AiOutlineHeart />
-
-<FaBeer />
-<FaBeer /> */}
-<FavoriteBorderIcon/> ---
-bag ----offers
+    <Box
+      sx={{
+        display: "flex",
+        // justifyContent : "space-between",
+        minWidth: "90px",
+        gap: "30px",
+        alignItems: "center",
+      }}
+      // border={"1px solid red"}
+    >
+      
+      <Link href="/wishlist">
+        <FavoriteBorderIcon />
+      </Link>
+      <Link href="/cart">
+        <ShoppingBagOutlinedIcon />
+      </Link>
+      <Link href="/offers">
+        <DiscountOutlinedIcon />
+      </Link>
     </Box>
-  )
-}
+  );
+};
 
-export default List
+export default List;
