949b73a266c0a5fe6e79290374722692668f0ddf
cs2812 <cs786786786@gmail.com>
2022-10-01 09:37:45 +0530

    CRUD

--- pages/api/cart/patch.js
+++ pages/api/cart/patch.js
@@ -1,5 +1,35 @@
 import CartModel from "../../../models/cart";
+import User from "../../../models/User";
+import jwt from "jsonwebtoken";
+import dbConnect from "../../../lib/mongodbConnect";
 
 const handler = async(req,res)=>{
-    res.send("ok")
+    if (req.method !== "PATCH") {
+		return res.send({
+			message: `${req.method} is not supported on this route.`,
+		});
+	}
+    const id=req.params.id;
+    const token= req.headers
+    try{
+
+        await dbConnect();
+        
+        let verify = jwt.verify( token , "ACCESSSECRET1234" )
+        
+        if(verify){
+            let decode = jwt.decode(token)
+
+            await CartModel.findByIdAndUpdate( id , req.body, { new: true } )
+    
+            return res.send.status(201).send("OK")
+        }
+        return res.status(401).send("Token Expired")
+    }
+    catch(e){
+    res.sataus(501).send({message:"Inernal error"})
+    }
+
 }
+
+export default handler;
