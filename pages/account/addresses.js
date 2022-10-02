6419d175a4246ea3d0b9a1975cc9bab7893e1e4a
PankajkandpalL <pankajkandpal5602@gmail.com>
2022-10-02 19:15:48 +0530

    updated some changes

--- pages/account/addresses.js
+++ pages/account/addresses.js
@@ -5,6 +5,7 @@ import { Button, Box } from "@mui/material";
 import AddAddress from "../../components/account/addAddress";
 import React from 'react';
 import AddCard from "../../components/account/addCard";
+import Layout from "../../components/Layout/Layout";
 
 
 export default function Address (){
@@ -48,25 +49,27 @@ export default function Address (){
         handleClose
     };
     return (
-        <div style={{padding:'2%',backgroundColor:'#f2f2f2'}}>
-            <Grid container spacing={{ xs: 2, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                <Grid item xs={3}>
-                    <AccountNavigation nav={router.pathname}/>
-                </Grid>
-                <Grid item xs={9}>
-                    <Box sx={{display:'flex', justifyContent:'space-between'}}>
-                        <Box></Box>
-                        <Button sx={{bgcolor:'black',borderRadius:'10px','&:hover':{bgcolor:'black'}}} variant='contained' onClick={handleOpen}>ADD NEW ADDRESS</Button>
-                        <AddAddress {...props}/>
-                    </Box>
-                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                        {addresses.map((add)=><Grid key={add.id} item xs={4}>
-                            <AddCard address={add} handleEdit={handleEdit} handleDelete={handleDelete}/>
-                        </Grid>)}
-                        
+        <Layout>
+            <div style={{padding:'2%',backgroundColor:'#f2f2f2'}}>
+                <Grid container spacing={{ xs: 2, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    <Grid item xs={3}>
+                        <AccountNavigation nav={router.pathname}/>
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Box sx={{display:'flex', justifyContent:'space-between'}}>
+                            <Box></Box>
+                            <Button sx={{bgcolor:'black',borderRadius:'10px','&:hover':{bgcolor:'black'}}} variant='contained' onClick={handleOpen}>ADD NEW ADDRESS</Button>
+                            <AddAddress {...props}/>
+                        </Box>
+                        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                            {addresses.map((add)=><Grid key={add.id} item xs={4}>
+                                <AddCard address={add} handleEdit={handleEdit} handleDelete={handleDelete}/>
+                            </Grid>)}
+                            
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-        </div>
+            </div>
+        </Layout>
     )
 }
