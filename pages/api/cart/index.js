8dbfe95d05296c5a28db2016caa8e2b79800816c
PankajkandpalL <pankajkandpal5602@gmail.com>
2022-10-02 09:39:43 +0530

    implemented crud on cart

--- pages/api/cart/index.js
+++ pages/api/cart/index.js
@@ -1,16 +1,33 @@
 import CartModel from "../../../models/cart";
 import dbConnect from "../../../lib/mongodbConnect";
+import jwt from "jsonwebtoken";
+import User from "../../../models/User";
 
-const handler=async(req,res)=>{
-    if(req.method!=="GET"){
-        res.send({
-            message:`${req.method} is not supported on this route`
-        })
+const handler = async (req, res) => {
+  if (req.method !== "GET") {
+    res.send({
+      message: `${req.method} is not supported on this route`,
+    });
+  }
+
+  let { token } = req.headers;
+
+  try {
+    await dbConnect();
+    let verify = jwt.verify(token, "ACCESSSECRET1234");
+    if (verify) {
+      let decode = jwt.decode(token);
+      let data = await User.findOne({ _id: decode.id })
+        .select({ _id: 0, mobile: 0, addresses: 0, __v: 0, email: 0 })
+        .populate("cartData")
+        .select({ price: 0, poster: 0 });
+      res.status(201).send({ cart: data });
     }
-    
-	await dbConnect();
-    let response= await CartModel.find()
-    res.send(response);
-}
+
+    return res.status(401).send("Unauthorized or Token Expired.");
+  } catch (e) {
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 export default handler;
--- pages/api/cart/patch.js
+++ pages/api/cart/patch.js
@@ -3,33 +3,37 @@ import User from "../../../models/User";
 import jwt from "jsonwebtoken";
 import dbConnect from "../../../lib/mongodbConnect";
 
-const handler = async(req,res)=>{
-    if (req.method !== "PATCH") {
-		return res.send({
-			message: `${req.method} is not supported on this route.`,
-		});
-	}
-    const id=req.params.id;
-    const token= req.headers
-    try{
+const handler = async (req, res) => {
+  if (req.method !== "PATCH") {
+    return res.send({
+      message: `${req.method} is not supported on this route.`,
+    });
+  }
+  const { id, quan } = req.body;
+  const { token } = req.headers;
+  try {
+    await dbConnect();
 
-        await dbConnect();
-        
-        let verify = jwt.verify( token , "ACCESSSECRET1234" )
-        
-        if(verify){
-            let decode = jwt.decode(token)
+    let verify = jwt.verify(token, "ACCESSSECRET1234");
 
-            await CartModel.findByIdAndUpdate( id , req.body, { new: true } )
-    
-            return res.send.status(201).send("OK")
-        }
-        return res.status(401).send("Token Expired")
+    console.log("Verify", verify);
+
+    if (verify) {
+      let decode = jwt.decode(token);
+
+      await User.findByIdAndUpdate(decode.id, { $pull: { cartData: id } });
+      await CartModel.findByIdAndUpdate(id, { $set: { quantity: quan } });
+      await User.findByIdAndUpdate(decode.id, { $push: { cartData: id } });
+      let data = await User.findById(decode.id)
+        .select({ mobile: 0, addresses: 0, __v: 0, email: 0 })
+        .populate("cartData");
+
+      return res.status(201).send({ updatedCart: data });
     }
-    catch(e){
-    res.sataus(501).send({message:"Inernal error"})
-    }
-
-}
+    return res.status(401).send("Token Expired");
+  } catch (e) {
+    res.status(501).send({ message: "Internal error" });
+  }
+};
 
 export default handler;
